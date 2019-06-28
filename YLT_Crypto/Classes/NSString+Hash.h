45699f07621cba22aae5dc72c163750a9d7021f9
项工 <xianggong@anve.com>
2019-06-28 10:01:45 +0800

    no message

--- YLT_Crypto/Classes/NSString+Hash.h
+++ YLT_Crypto/Classes/NSString+Hash.h
@@ -21,7 +21,7 @@
 /**
  SHA1 加密
  * 终端命令测试
- * echo -n "string" | openssl sha -sha1
+ * echo -n "string" | openssl dgst -sha1
  @return 加密后字串
  */
 - (NSString *)sha1String;
@@ -30,7 +30,7 @@
  计算SHA256散列结果
  *
  * 终端测试命令：
- * echo -n "string" | openssl sha -sha256
+ * echo -n "string" | openssl dgst -sha256
  @return 加密后字串
  */
 - (NSString *)sha256String;
@@ -39,7 +39,7 @@
  计算SHA 512散列结果
  *
  * 终端测试命令：
- * echo -n "string" | openssl sha -sha512
+ * echo -n "string" | openssl dgst -sha512
  @return 128个字符的SHA 512散列字符串
  */
 - (NSString *)sha512String;
@@ -58,7 +58,7 @@
  计算HMAC SHA1散列结果
  *
  * 终端测试命令：
- * echo -n "string" | openssl sha -sha1 -hmac "key"
+ * echo -n "string" | openssl dgst -sha1 -hmac "key"
  @return 40个字符的HMAC SHA1散列字符串
  */
 - (NSString *)hmacSHA1StringWithKey:(NSString *)key;
@@ -67,7 +67,7 @@
  计算HMAC SHA256散列结果
  *
  * 终端测试命令：
- * echo -n "string" | openssl sha -sha256 -hmac "key"
+ * echo -n "string" | openssl dgst -sha256 -hmac "key"
  @return 64个字符的HMAC SHA256散列字符串
  */
 - (NSString *)hmacSHA256StringWithKey:(NSString *)key;
@@ -76,7 +76,7 @@
  计算HMAC SHA512散列结果
  *
  * 终端测试命令：
- * echo -n "string" | openssl sha -sha512 -hmac "key"
+ * echo -n "string" | openssl dgst -sha512 -hmac "key"
  @return 128个字符的HMAC SHA512散列字符串
  */
 - (NSString *)hmacSHA512StringWithKey:(NSString *)key;
@@ -106,7 +106,7 @@
  计算文件的SHA256散列结果
  *
  * 终端测试命令：
- * openssl sha -sha256 file.dat
+ * openssl dgst -sha256 file.dat
  @return 64个字符的SHA256散列字符串
  */
 - (NSString *)fileSHA256Hash;
@@ -115,7 +115,7 @@
  计算文件的SHA512散列结果
  *
  * 终端测试命令：
- * openssl sha -sha512 file.dat
+ * openssl dgst -sha512 file.dat
  @return 128个字符的SHA512散列字符串
  */
 - (NSString *)fileSHA512Hash;
